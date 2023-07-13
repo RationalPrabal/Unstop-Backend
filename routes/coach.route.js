3933623ea7340c0aea16072e62209c6a7cdd14b5
Prabal <prabal19999@gmail.com>
2023-07-14 02:45:22 +0530

    tweaked some logic

--- routes/coach.route.js
+++ routes/coach.route.js
@@ -50,73 +50,65 @@ coachRouter.patch("/bookSeat",async(req,res)=>{
        
         const numberOfSeats= req.body.seats;
        
-        const emptySeats=await seatModel.find({isBooked:false})
-        function allocateSeats(numSeats) {
-            let allocatedSeats = [];
-          
-            // Check if there are enough seats available in one row
-            const row = findRowWithAvailableSeats(numSeats);
-            if (row !== -1) {
-              allocateSeatsInRow(numSeats, row);
-            } else {
-              // If there are no available seats in one row, allocate seats across multiple rows
-              allocateSeatsAcrossRows(numSeats);
-            }
-          
-            // Function to find a row with available seats
-            function findRowWithAvailableSeats(seatsNeeded) {
-              let row = -1;
-              for (let i = 0; i < emptySeats.length; i++) {
-                const seat = emptySeats[i];
-                if (seat.isBooked === false && seat.seatNumber + seatsNeeded <= 8) {
-                  row = seat.rowNumber;
-                  break;
-                }
-              }
-              return row;
-            }
-          
-            // Function to allocate seats in a specific row
-            function allocateSeatsInRow(seatsNeeded, row) {
-              const rowSeats = emptySeats.filter((seat) => seat.rowNumber === row);
-              const availableSeats = rowSeats.filter((seat) => seat.isBooked === false);
-              if (availableSeats.length >= seatsNeeded) {
-                for (let i = 0; i < seatsNeeded; i++) {
-                  const seat = availableSeats[i];
-                  allocatedSeats.push({ seatNumber: seat.seatNumber, rowNumber: seat.rowNumber ,_id:seat._id});
-                  seat.isBooked = true;
-                }
-        
-              } else {
-              
-                allocateSeatsAcrossRows(seatsNeeded);
-              }
-            }
-          
-            // Function to allocate seats across multiple rows
-            function allocateSeatsAcrossRows(seatsNeeded) {
-              let remainingSeats = emptySeats.filter((seat) => seat.isBooked === false);
-              for (let i = 0; i < remainingSeats.length; i++) {
-                const seat = remainingSeats[i];
-                if (allocatedSeats.length === seatsNeeded) {
-                  break;
-                }
-                if (seat.isBooked === false) {
-                  allocatedSeats.push({ seatNumber: seat.seatNumber, rowNumber: seat.rowNumber,_id:seat._id});
-                  seat.isBooked = true;
-                }
-              }
-          
-              if (allocatedSeats.length < seatsNeeded) {
-                console.log(`Insufficient seats available. Only ${allocatedSeats.length} seats allocated.`);
-              } else {
-                console.log(`Seats ${allocatedSeats.map((seat) => `${seat.seatNumber} (Row ${seat.rowNumber})`).join(', ')} allocated as near as possible.`);
-              }
-            }
-          
-            return allocatedSeats;
-          }
+        const emptySeats = await seatModel.find({ isBooked: false });
 
+        function allocateSeats(numSeats) {
+          let allocatedSeats = [];
+        
+          const row = findRowWithAvailableSeats(numSeats);
+          if (row !== -1) {
+            allocateSeatsInRow(numSeats, row);
+          } else {
+            allocateSeatsAcrossRows(numSeats);
+          }
+        
+          function findRowWithAvailableSeats(seatsNeeded) {
+            let row = -1;
+            for (let i = 0; i < emptySeats.length; i++) {
+              const seat = emptySeats[i];
+              if (seat.isBooked === false && seat.seatNumber + seatsNeeded <= 8) {
+                row = seat.rowNumber;
+                break;
+              }
+            }
+            return row;
+          }
+        
+          function allocateSeatsInRow(seatsNeeded, row) {
+            const rowSeats = emptySeats.filter((seat) => seat.rowNumber === row);
+            const availableSeats = rowSeats.filter((seat) => seat.isBooked === false);
+            if (availableSeats.length >= seatsNeeded) {
+              for (let i = 0; i < seatsNeeded; i++) {
+                const seat = availableSeats[i];
+                allocatedSeats.push({ seatNumber: seat.seatNumber, rowNumber: seat.rowNumber, _id: seat._id });
+                seat.isBooked = true;
+              }
+            } else {
+              allocateSeatsAcrossRows(seatsNeeded);
+            }
+          }
+        
+          function allocateSeatsAcrossRows(seatsNeeded) {
+            const sortedSeats = emptySeats
+              .filter((seat) => seat.isBooked === false)
+              .sort((a, b) => a.seatNumber - b.seatNumber);
+        
+            let seatIndex = 0;
+            while (allocatedSeats.length < seatsNeeded && seatIndex < sortedSeats.length) {
+              const seat = sortedSeats[seatIndex];
+              allocatedSeats.push({ seatNumber: seat.seatNumber, rowNumber: seat.rowNumber, _id: seat._id });
+              seat.isBooked = true;
+              seatIndex++;
+            }
+          }
+        
+          return allocatedSeats;
+        }
+        
+    
+
+
+        
           const seatNumbers =allocateSeats(numberOfSeats);
           const allocatedSeatIds = seatNumbers.map((seat) => seat._id);
 
